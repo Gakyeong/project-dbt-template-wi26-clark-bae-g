815a951679bf6ced51646be765db8f8bb892356c
Gakyeong <gakyeongbae@gmail.com>
2026-03-23 16:05:58 -0600

    columnnamechagned

--- itm327_dbt_bae_g/models/staging/raw_weather.sql
+++ itm327_dbt_bae_g/models/staging/raw_weather.sql
@@ -1,16 +1,15 @@
--- TODO: Update the source table name to match your prefix (e.g., SMITHJ_WEATHER)
 select
-    DATE as raw_date,
-    CITY,
-    MAX_TEMP,
-    MIN_TEMP,
-    PRECIP,
-    MAX_WIND,
-    APPT_MAX,
-    APPT_MIN,
-    APPT_TEMP_RANGE,
-    TEMP_RANGE_C,
-    DAYLIGHT_DURATION,
-    SUNRISE,
-    SUNSET
+    "date" as raw_date,
+    "city",
+    "max_temp",
+    "min_temp",
+    "precip",
+    "max_wind",
+    "appt_max",
+    "appt_min",
+    "appt_temp_range",
+    "temp_range_c",
+    "daylight_duration",
+    "sunrise",
+    "sunset"
 from {{ source('snowbearair', 'WEATHER_API_BAE_G') }}
