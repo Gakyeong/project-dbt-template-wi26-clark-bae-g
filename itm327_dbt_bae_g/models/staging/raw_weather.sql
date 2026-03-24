224b0b1dd47d2d388735b71a6a2a8c6690f05ca8
Gakyeong <gakyeongbae@gmail.com>
2026-03-24 11:23:00 -0600

    column

--- itm327_dbt_bae_g/models/staging/raw_weather.sql
+++ itm327_dbt_bae_g/models/staging/raw_weather.sql
@@ -1,15 +1,15 @@
 select
-    "date" as raw_date,
-    "city",
-    "max_temp",
-    "min_temp",
-    "precip",
-    "max_wind",
-    "appt_max",
-    "appt_min",
-    "appt_temp_range",
-    "temp_range_c",
-    "daylight_duration",
-    "sunrise",
-    "sunset"
+    date as raw_date,
+    city,
+    max_temp,
+    min_temp,
+    precip,
+    max_wind,
+    appt_max,
+    appt_min,
+    appt_temp_range,
+    temp_range_c,
+    daylight_duration,
+    sunrise,
+    sunset
 from {{ source('snowbearair', 'WEATHER_API_BAE_G') }}
